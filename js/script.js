ea11b7fcbdd9140b0af516d41930710ae48213fc
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-13 23:18:00 -0500

    feature(main): add reddit lane

--- js/script.js
+++ js/script.js
@@ -5,10 +5,29 @@ const addRedditbutton = document.createElement("button");
 addRedditbutton.innerHTML = '<i class="fa-solid fa-plus"></i>';
 addRedditbutton.classList.add("add-subreddit-button");
 buttonContainer.appendChild(addRedditbutton);
-mainContainer.appendChild(buttonContainer)
+mainContainer.appendChild(buttonContainer);
+
+const errors = {
+    netWorkError:"sub reddit doesn't exist."
+};
+
+async function getSubRedditData(subReddit){
+    try{
+        const request = await fetch(`https://www.reddit.com/r/${subReddit}.json`);
+        const result = await request.json();
+        if(result.data.children.length){
+            return result.data.children;
+        }else{
+            return {...result};
+        }
+    }catch(error){
+        return errors.netWorkError;
+    }
+}
+
 addRedditbutton.addEventListener("click",()=>{
-    console.log(buttonContainer.childNodes);
     if(buttonContainer.childElementCount===1){
+        // create menu
         const addRedditMenu = document.createElement("div");
         addRedditMenu.classList.add("add-reddit-menu");
         const inputLabel = document.createElement("label");
@@ -24,5 +43,37 @@ addRedditbutton.addEventListener("click",()=>{
         addRedditMenu.appendChild(inputLabel);
         addRedditMenu.appendChild(subRedditInput);
         addRedditMenu.appendChild(addSubRedditButton);
+
+        addSubRedditButton.addEventListener("click",async ()=>{
+            const subRedditData = await getSubRedditData(subRedditInput.value);
+            if(subRedditData===errors.netWorkError){
+                console.log("no existe el subreddit");
+            }else{
+                const newRedditLane = document.createElement("div");
+                
+                const newRedditLaneHeader = document.createElement("div");
+                const headerTitle = document.createElement("span");
+                headerTitle.textContent = `r/${subRedditInput.value}`;
+                newRedditLaneHeader.appendChild(headerTitle);
+
+                subRedditData.forEach(post=>{
+                    const newPost = document.createElement("div");
+                    const postTitle = document.createElement("span");
+                    postTitle.textContent = post.data.title;
+
+                    newPost.appendChild(postTitle);
+                    newRedditLane.appendChild(newPost);
+
+                });
+
+                
+                newRedditLane.appendChild(newRedditLaneHeader);
+
+                mainContainer.innerHTML = "";
+
+                mainContainer.appendChild(newRedditLane);
+                mainContainer.appendChild(buttonContainer);
+            }
+        });
     }
 });
